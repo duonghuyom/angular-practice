7eb197ed847eef6dd30af851be24f7190cffa715
duonghuyom <huy.nguyen20@onemount.com>
2022-09-14 11:20:28 +0700

    data binding - event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-practice';
+  name = 'Duong Huy';
+  age = '21';
+
+  xinChao() {
+    alert('hello user');
+  }
+
+  authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+    // more data
+  ];
 }
